d129136f5e17b854aaef8a7486722602ce0308c6
vladislav149 <vladislav149@mail.ru>
2022-04-11 23:49:03 +0300

    fixed 1 row

--- js/consts.js
+++ js/consts.js
@@ -1,4 +1,4 @@
-export const UI_ELEMENTS = {
+const UI_ELEMENTS = {
   FORM: document.querySelector('.main__form'),
   INPUT: document.querySelector('.main__input'),
   OUTPUT: document.querySelector('.main__output'),
